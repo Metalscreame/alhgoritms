c46ab62222a8dac6be1ba303206ae17010931ef8
Roman <mantilocal@gmail.com>
2020-06-11 15:36:24 +0300

    added some comments

--- sort/bsch.go
+++ sort/bsch.go
@@ -7,22 +7,30 @@ func bsrch(nums []int, target int) int {
 	if len(nums) == 0 {
 		return -1
 	}
+	// as this aray is sorted we can find value in the miidle and undestand in which part of array
+	//  we can find target value. we wil be doing this dividing by two until we find targt value
+	// or until we chek all parts of array
+	low, high := 0, len(nums)-1 // lets define two indexes of a slice
+	mid := 0 //and define mid variable
 
-	low, high := 0, len(nums)-1
-	mid := 0
-	for low <= high {
-		mid = (high + low) >> 1 // or (high+low)/2
-		if nums[mid] == target {
+	for low <= high { // we will be iterating until indexes are in aproptiate states.
+	// so until low pos is less or equals to high
+	// so first lets find mid value
+		mid = (high + low) >> 1 // or (high+low)/2. this is right shift operator
+		if nums[mid] == target { // and then lets check if the the value by middle index is the target
 			return mid
 		}
-
+		// if not lets check whether mid val is less then target value
+		// if yes - then we cut first half of an array
 		if nums[mid] < target {
-			low = mid + 1
+			low = mid + 1 // and make low index as mid +1. as mid ind is already checked
 			continue
 		}
-
+		// if no this means that target is in low part and we need to cut hight part
 		high = mid - 1
 	}
+
+	// if target is still not found - return that it is not found
 	return -1
 }
 
